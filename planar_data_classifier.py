910178fcff8778c2efa2a42c255b9451cb0ded0c
mjkshitiz <kshitizmaharjan@gmail.com>
2021-11-30 18:27:48 +0545

    Modified for cleaner code

--- planar_data_classifier.py
+++ planar_data_classifier.py
@@ -17,12 +17,12 @@ plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
 
 # training set size
 # m = ...
-# YOUR CODE STARTS HERE
+
 shape_X = X.shape
 shape_Y = Y.shape
 m = shape_X[1]
 
-# YOUR CODE ENDS HERE
+
 
 print ('The shape of X is: ' + str(shape_X))
 print ('The shape of Y is: ' + str(shape_Y))
@@ -56,12 +56,12 @@ def layer_sizes(X, Y):
     n_y -- the size of the output layer
     """
 
-    # YOUR CODE STARTS HERE
+    
     n_x = X.shape[0]
     n_h = 4
     n_y = Y.shape[0]
     
-    # YOUR CODE ENDS HERE
+    
     return (n_x, n_h, n_y)
 
 t_X, t_Y = layer_sizes_test_case()
@@ -89,18 +89,12 @@ def initialize_parameters(n_x, n_h, n_y):
                     W2 -- weight matrix of shape (n_y, n_h)
                     b2 -- bias vector of shape (n_y, 1)
     """    
-    #(≈ 4 lines of code)
-    # W1 = ...
-    # b1 = ...
-    # W2 = ...
-    # b2 = ...
-    # YOUR CODE STARTS HERE
     W1 = np.random.randn(n_h,n_x)*0.01
     b1 = np.zeros((n_h,1))
     W2 = np.random.randn(n_y,n_h)*0.01
     b2 = np.zeros((n_y,1))
     
-    # YOUR CODE ENDS HERE
+    
 
     parameters = {"W1": W1,
                   "b1": b1,
@@ -133,22 +127,19 @@ def forward_propagation(X, parameters):
     cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
     """
     # Retrieve each parameter from the dictionary "parameters"
-    # YOUR CODE STARTS HERE
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
-    # YOUR CODE ENDS HERE
     
     # Implement Forward Propagation to calculate A2 (probabilities)
-
-    # YOUR CODE STARTS HERE
+    
     Z1 = np.dot(W1,X) + b1
     A1 = np.tanh(Z1)
     Z2 = np.dot(W2,A1) + b2
     A2 = sigmoid(Z2)
     
-    # YOUR CODE ENDS HERE
+    
     
     assert(A2.shape == (1, X.shape[1]))
     
@@ -184,10 +175,9 @@ def compute_cost(A2, Y):
 
     # Compute the cross-entropy cost
 
-    # YOUR CODE STARTS HERE
     logprobs = np.multiply(np.log(A2),Y)+(1-Y)*np.log(1-A2)
     cost = -np.sum(logprobs)/m
-    # YOUR CODE ENDS HERE
+    
     
     cost = float(np.squeeze(cost))  # makes sure cost is the dimension we expect. 
                                     # E.g., turns [[17]] into 17 
@@ -219,30 +209,24 @@ def backward_propagation(parameters, cache, X, Y):
     m = X.shape[1]
     
     # First, retrieve W1 and W2 from the dictionary "parameters".
-
-    # YOUR CODE STARTS HERE
+  
     W1 = parameters["W1"]
     W2 = parameters["W2"]
-    
-    # YOUR CODE ENDS HERE
-        
+       
     # Retrieve also A1 and A2 from dictionary "cache".
 
-    # YOUR CODE STARTS HERE
     A1 = cache["A1"]
     A2 = cache["A2"]
-    # YOUR CODE ENDS HERE
     
     # Backward propagation: calculate dW1, db1, dW2, db2. 
-
-    # YOUR CODE STARTS HERE
+    
     dZ2 = A2-Y
     dW2 = (1/m)*np.dot(dZ2,A1.T)
     db2 = (1/m)*np.sum(dZ2,axis=1,keepdims=True)
     dZ1 = np.dot(W2.T,dZ2)*(1-np.power(A1,2))
     dW1 = (1/m)*np.dot(dZ1,X.T)
     db1 = (1/m)*np.sum(dZ1,axis=1,keepdims=True)
-    # YOUR CODE ENDS HERE
+    
     
     grads = {"dW1": dW1,
              "db1": db1,
@@ -275,32 +259,29 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
     parameters -- python dictionary containing your updated parameters 
     """
     # Retrieve a copy of each parameter from the dictionary "parameters". Use copy.deepcopy(...) for W1 and W2
-
-    # YOUR CODE STARTS HERE
+    
     W1 = copy.deepcopy(parameters["W1"])
     b1 = parameters["b1"]
     W2 = copy.deepcopy(parameters["W2"])
     b2 = parameters["b2"]
-    # YOUR CODE ENDS HERE
+    
     
     # Retrieve each gradient from the dictionary "grads"
-
-    # YOUR CODE STARTS HERE
+    
     dW1 = grads["dW1"]
     db1 = grads["db1"]
     dW2 = grads["dW2"]
     db2 = grads["db2"]
-    # YOUR CODE ENDS HERE
+    
     
     # Update rule for each parameter
-
-    # YOUR CODE STARTS HERE
+    
     W1 = W1-learning_rate*dW1
     b1 = b1-learning_rate*db1
     W2 = W2-learning_rate*dW2
     b2 = b2-learning_rate*db2
     
-    # YOUR CODE ENDS HERE
+    
     
     parameters = {"W1": W1,
                   "b1": b1,
@@ -320,7 +301,7 @@ print("b2 = " + str(parameters["b2"]))
 
 update_parameters_test(update_parameters)
 
-# GRADED FUNCTION: nn_model
+#  FUNCTION: nn_model
 
 def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
     """
@@ -340,13 +321,9 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
     n_y = layer_sizes(X, Y)[2]
     
     # Initialize parameters
-    #(≈ 1 line of code)
-    # parameters = ...
-    # YOUR CODE STARTS HERE
+
     parameters = initialize_parameters(n_x,n_h,n_y)
-    
-    # YOUR CODE ENDS HERE
-    
+   
     # Loop (gradient descent)
 
     for i in range(0, num_iterations):
@@ -364,12 +341,12 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
         # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
         # parameters = ...
         
-        # YOUR CODE STARTS HERE
+        
         A2, cache = forward_propagation(X,parameters)
         cost = compute_cost(A2,Y)
         grads = backward_propagation(parameters,cache,X,Y)
         parameters = update_parameters(parameters,grads)
-        # YOUR CODE ENDS HERE
+        
         
         # Print the cost every 1000 iterations
         if print_cost and i % 1000 == 0:
@@ -388,7 +365,7 @@ print("b2 = " + str(parameters["b2"]))
 
 nn_model_test(nn_model)
 
-# GRADED FUNCTION: predict
+# FUNCTION: predict
 
 def predict(parameters, X):
     """
@@ -406,10 +383,10 @@ def predict(parameters, X):
     #(≈ 2 lines of code)
     # A2, cache = ...
     # predictions = ...
-    # YOUR CODE STARTS HERE
+    
     A2, cache = forward_propagation(X,parameters)
     predictions = A2 >0.5
-    # YOUR CODE ENDS HERE
+    
     
     return predictions
 
